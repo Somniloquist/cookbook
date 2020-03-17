d8ad5d068ad70100ea3e692dd4e07b0db6265e2f
Robin Unger <robin.e.unger@gmail.com>
2020-03-16 21:28:51 -0500

    Add togglable sidebar

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,20 @@
+function expandNav() {
+  var nav = document.getElementById("mobile-nav");
+  var closeArea = document.getElementById("mobile-nav-close"); // allows us to click outside the menu to close it
+  nav.style.visibility = "visible";
+  nav.style.width = "250px";
+  closeArea.style.visibility = "visible";
+  closeArea.style.background = "rgba(0,0,0,0.65)"
+}
+
+function closeNav() {
+  var nav = document.getElementById("mobile-nav");
+  var closeArea = document.getElementById("mobile-nav-close"); // allows us to click outside the menu to close it
+  nav.style.width = "0";
+  closeArea.style.background = "rgba(0,0,0,0)"
+  // give the transition time to complete
+  setTimeout(() => { 
+    closeArea.style.visibility = "hidden";
+    nav.style.visibility = "hidden";
+  }, 400);
+}
