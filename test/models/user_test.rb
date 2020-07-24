3424f387e7fde0d7f639c4aad766decc0f80d6f7
Robin Unger <robin.e.unger@gmail.com>
2020-07-23 20:07:14 -0500

    Add username to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:artoria)
+  end
+
+  test "user should be valid" do
+    assert @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = ""
+    assert_not @user.valid?
+  end
+
+  test "username should be at least 4 characters" do
+    @user.username = "a" * 3
+    assert_not @user.valid?
+  end
+
+  test "username should not exceed 25 characters" do
+    @user.username = "a" * 26
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
 end
